49a7684e9502b44ca57cedff91c1e7f3c5ec9d6f
Ivan D. Velez <ivancho175@hotmail.com>
2022-02-28 11:42:42 -0500

    ---

--- src/app/@shared/services/files.service.ts
+++ src/app/@shared/services/files.service.ts
@@ -1,15 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import firebase from 'firebase/compat';
-import 'firebase/compat/storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilesService {
-
-  storageRef = firebase.app().storage().ref();
 
   constructor(
     private http: HttpClient
